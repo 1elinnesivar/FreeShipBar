8d0c1fbf1079b105b65c57797134bddcc5815fbc
Zekayi Özdemir <zekayiozdemir16@gmail.com>
2025-11-10 16:25:44 +0300

    Update sitemap URL in robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -11,7 +11,7 @@ export default function robots(): MetadataRoute.Robots {
         disallow: ['/api/', '/embed', '/admin'],
       },
     ],
-    sitemap: `${baseUrl}/sitemap_v1.xml`,
+    sitemap: `${baseUrl}/sitemap.xml`,
   }
 }
 
